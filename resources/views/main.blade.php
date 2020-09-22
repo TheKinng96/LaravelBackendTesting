571e7daf7b04cfc2bcf205cb3897083e2d87ac33
TheKinng96 <fengyuan.yap@example.com>
2020-09-22 21:09:29 +0900

    added db for products and doing product page

---
+++ resources/views/main.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Laravel 7 Home Page</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+</head>
+
+<body>
+  <div class="container">
+    <div class="row">
+      <h1>Add Your Product</h1>
+      <a href="" class="btn btn-success">Add Your Product</a>
+    </div>
+  </div>
+
+</body>
+
+</html>
